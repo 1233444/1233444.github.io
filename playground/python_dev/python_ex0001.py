eea84745446fe425b42c4256d5ef6a2fc43ad626
1233444 <1233444@gmail.com>
2020-04-29 17:50:53 -0400

    commented the python code

--- playground/python_dev/python_ex0001.py
+++ playground/python_dev/python_ex0001.py
@@ -1,3 +1,13 @@
 import sys
-print(sys.executable)
-print(sys.version)
+# print(sys.executable)
+# print(sys.version)
+
+def greet(who_to_greet):
+    greeting = 'Hello, {}'.format(who_to_greet)
+    return greeting
+
+
+
+
+print(greet('World'))
+print(greet('Charlie'))
